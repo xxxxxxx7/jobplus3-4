45209416b8a98c1d7f800458c5ca21289e0672c8
微笑领悟_默默坚强 <ldmoko@googlemail.com>
2018-01-18 16:28:12 +0800

    fix #16 add base blueprint (#25)
    * fix #13 添加Flask APP基本应用运行
    
    * correct defautl to default
    
    * fix #16 add base blueprint

--- jobplus/app.py
+++ jobplus/app.py
@@ -8,6 +8,12 @@ def create_app(config):
     app.config.from_object(configs.get(config))
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 0
 
+    register_blueprints(app)
     return app
 
+
+def register_blueprints(app):
+    from .handlers import front
+
+    app.register_blueprint(front)
 
